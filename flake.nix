19b718f79cc3a34f13ebfbd4b18829e2c3612d02
Your Name <you@example.com>
2024-04-03 18:36:28 +0000

    Switch kafka to redpanda

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
   inputs.organist.inputs.nixpkgs.follows = "nixpkgs";
   inputs.organist.inputs.nickel_src.follows = "nickel_src";
 
-  nixConfig = {
-    extra-substituters = ["https://organist.cachix.org" "https://nix-community.cachix.org"];
-    extra-trusted-public-keys = ["organist.cachix.org-1:GB9gOx3rbGl7YEh6DwOscD1+E/Gc5ZCnzqwObNH2Faw=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
-  };
+#  nixConfig = {
+#    extra-substituters = ["https://organist.cachix.org" "https://nix-community.cachix.org"];
+#    extra-trusted-public-keys = ["organist.cachix.org-1:GB9gOx3rbGl7YEh6DwOscD1+E/Gc5ZCnzqwObNH2Faw=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+#  };
 
   outputs = {organist, ...} @ inputs:
     organist.flake.outputsFromNickel ./. inputs {};
